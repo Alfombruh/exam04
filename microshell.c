fc813e6d5f997c006788c65f8f32fc81519a730d
Joaquin Fernandez <jofernan@c3r8s4.42urduliz.com>
2022-10-31 14:18:27 +0100

    finished

--- microshell.c
+++ microshell.c
@@ -5,14 +5,34 @@
 #include <stdlib.h>
 
 //tiene que es
-static int ft_countcmd(char **argv){
+
+static int	ft_strlen(char *str){
 	int i = -1;
-	int count = 0;
-	while(argv[++i]){
-		if (!strcmp(argv[i], "|") || !strcmp(argv[i], ";"))
-			count++;
+	if (!str)
+		return 0;
+	while(str[++i])
+		continue;
+	return i;
+}
+
+static int error_message(char *msg){
+	write(2, msg, ft_strlen(msg));
+	write(2, "\n", sizeof(char));
+	return 1;
+}
+
+static void	execute_cd(char **args){
+	int i = -1;
+	while (args[++i] != NULL)
+		continue;
+	if (i != 2) // arreglar eso
+		exit (error_message("error: bad arguments"));
+	if (chdir(args[1]) == -1){
+		write(2, "error: cannot change directory to " , sizeof("error: cannot change directory to "));
+		write(2, args[1], ft_strlen(args[1]));
+		write(2, "\n", 1);
 	}
-	return count;
+	exit (0);
 }
 
 int main(int argc, char **argv, char **env){
@@ -22,13 +42,11 @@ int main(int argc, char **argv, char **env){
 	int	last = 1; //if the last command was a pipe is 1 and if it was a calling its 0
 	int	fd[2][2];
 	if (argc == 1)
-		return 1;
+		return (1);
 	if (pipe(fd[0]))
-		return 1;
+		return (error_message("error: fatal"));
 	if (pipe(fd[1]))
-		return 1;
-	int	total = ft_countcmd(argv);
-	(void) total;
+		return (error_message("error: fatal"));
 	while (argv[++i]){
 		close(fd[0][1]);
 		if (!strcmp(argv[i],"|")){
@@ -43,8 +61,14 @@ int main(int argc, char **argv, char **env){
 				close(fd[1][0]);
 				close(fd[1][1]);
 				argv[i] = NULL;
-				execve(argv[cmd], &argv[cmd], env);
-				exit (0);
+				if (!strcmp(argv[cmd], "cd"))
+					execute_cd(&argv[cmd]);
+				if (execve(argv[cmd], &argv[cmd], env) == -1){
+					write(2, "error: cannot execute ", sizeof("error: cannot execute "));
+					write(2, argv[cmd] ,ft_strlen(argv[cmd]));
+					write(2, "\n", 1);
+				}
+				exit (1);
 			}
 			else
 				waitpid(pid,NULL, 0);
@@ -67,8 +91,14 @@ int main(int argc, char **argv, char **env){
 				close(fd[1][0]);
 				close(fd[1][1]);
 				argv[i] = NULL;
-				execve(argv[cmd], &argv[cmd], env);
-				exit (0);
+				if (!strcmp(argv[cmd], "cd"))
+					execute_cd(&argv[cmd]);
+				if (execve(argv[cmd], &argv[cmd], env) == -1){
+					write(2, "error: cannot execute ", sizeof("error: cannot execute "));
+					write(2, argv[cmd] ,ft_strlen(argv[cmd]));
+					write(2, "\n", 1);
+				}
+				exit (1);
 			}
 			else
 				waitpid(pid,NULL, 0);
@@ -80,8 +110,8 @@ int main(int argc, char **argv, char **env){
 			fd[0][1] = fd[1][1];
 			pipe(fd[1]);
 		}
-//		printf("fd[0][0] = %d || fd[0][1] = %d\n", fd[0][0],fd[0][1]);
-//		printf("fd[1][0] = %d || fd[1][1] = %d\n", fd[1][0],fd[1][1]);
+	//	printf("fd[0][0] = %d || fd[0][1] = %d\n", fd[0][0],fd[0][1]);
+	//	printf("fd[1][0] = %d || fd[1][1] = %d\n", fd[1][0],fd[1][1]);
 	}
 	++count;
 	int pid = fork();
@@ -93,8 +123,14 @@ int main(int argc, char **argv, char **env){
 		close(fd[1][0]);
 		close(fd[1][1]);
 		argv[i] = NULL;
-		execve(argv[cmd], &argv[cmd], env);
-		exit (0);
+		if (!strcmp(argv[cmd], "cd"))
+			execute_cd(&argv[cmd]);
+		if (execve(argv[cmd], &argv[cmd], env) == -1){
+			write(2, "error: cannot execute ", sizeof("error: cannot execute "));
+			write(2, argv[cmd] ,ft_strlen(argv[cmd]));
+			write(2, "\n", 1);
+		}
+		exit (1);
 	}
 	else
 		waitpid(pid,NULL, 0);
